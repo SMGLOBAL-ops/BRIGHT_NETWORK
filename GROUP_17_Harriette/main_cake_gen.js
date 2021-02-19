920e54673e12e2b1c882633b521b4bedc8431ba7
HarryLam14 <hlam0478@gmail.com>
2021-02-19 15:46:23 +0000

    aligning middle carousel image

--- GROUP_17_Harriette/main_cake_gen.js
+++ GROUP_17_Harriette/main_cake_gen.js
@@ -7,7 +7,7 @@
 function generateCake() {
     var image = document.createElement('img');
     var div = document.getElementById('flex-cake-gen');
-    let number = Math.floor((Math.random()*10) +1 );
+    let number = Math.floor((Math.random()*50) +1 );
     image.src = "https://loremflickr.com/320/240/cake?random="+number;;
     div.appendChild(image);
 }
